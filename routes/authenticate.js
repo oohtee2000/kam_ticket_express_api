6a6f466e77ea15ff96c2bd2c84c61adf16e83cb2
oohtee2000 <oluwatosinissa2000@gmail.com>
2025-04-24 20:53:46 +0100

    Switching bcrypt to bcryptjs

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { query } = require("../config/db");
 
 const router = express.Router();
